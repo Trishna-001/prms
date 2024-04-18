ab36ee4a02d9d189165b10233150e2a72a8b4a8f
Trishna-001 <167233399+Trishna-001@users.noreply.github.com>
2024-04-18 18:49:58 +0530

    content-of page

--- doctors/doctor-list-home.php
+++ doctors/doctor-list-home.php
@@ -7,6 +7,41 @@
 
     </head>
     <body>
-        <p>under construction</p>
+        <div id="content_outer">
+            <center>
+                <div id="content_inner">
+                    <div id="content">
+                    <p>Doctors View</p>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    Name of Doctor          
+		                        </th>
+                                <th>
+                                    Department          
+		                        </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="even">
+                                <td>Debika Jaswal</td>
+                                <td>Orthopedics</td>
+    
+                            </tr>
+                            <tr class="even">
+                                <td>Debika Jaswal</td>
+                                <td>Orthopedics</td>
+                            </tr>
+                            <tr class="even">
+                                <td>Debika Jaswal</td>
+                                <td>Orthopedics</td>
+                            </tr> 
+                        </tbody>
+                    </table>
+                </div>
+            </div>    
+        </center>
+</div>
     </body>
 </html>
